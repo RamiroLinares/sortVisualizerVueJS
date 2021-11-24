3e08bb0c99e03aa24d9ea0c1e0f364827be107fe
RamiroLinares <Ramiro.Linares@jala-foundation.org>
2021-11-24 16:36:49 -0300

    quicksort test

--- src/views/Quicksort.vue
+++ src/views/Quicksort.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="quicksort">
     <h1>Quicksort</h1>
-    <b-button variant='primary' v-on:click="quicksort">Quicksort</b-button>
+    <b-button variant='primary' v-on:click="quicksort()">Quicksort</b-button>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
   methods: {
     quicksort () {
       const array = [4000, 10, 24, 1]
-      quicksort(array)
+      console.log(quicksort(array))
     }
   }
 }
