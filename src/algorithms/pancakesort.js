2f522886bacaf4fd03e0d6e6e035ac272d61262a
RamiroLinares <Ramiro.Linares@jala-foundation.org>
2021-11-24 19:23:55 -0300

    added pancake and selection  algorithms

--- src/algorithms/pancakesort.js
+++ src/algorithms/pancakesort.js
@@ -1,4 +1,30 @@
 export default function pancakesort(array) {
-
-
-return "pancake";}
+    for (var i = array.length - 1; i >= 1; i--) {
+        // find the index of the largest element not yet sorted
+        var max_idx = 0;
+        var max = array[0];
+        for (var j = 1; j <= i; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                max_idx = j;
+            }
+        }
+ 
+        if (max_idx == i) 
+            continue; // element already in place
+ 
+        var new_slice;
+ 
+        // flip array max element to index 0
+        if (max_idx > 0) {
+            new_slice = array.slice(0, max_idx+1).reverse();
+            for ( j = 0; j <= max_idx; j++) 
+                array[j] = new_slice[j];
+        }
+ 
+        // then flip the max element to its place
+        new_slice = array.slice(0, i+1).reverse();
+        for ( j = 0; j <= i; j++) 
+            array[j] = new_slice[j];
+    }
+    return array;}
--- src/algorithms/selectionsort.js
+++ src/algorithms/selectionsort.js
@@ -1,4 +1,19 @@
 export default function selectionsort(array) {
-
-
-return "selection";}
+    let n = array.length;
+        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(array[j] < array[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             // Swapping the elements
+             let tmp = array[i]; 
+             array[i] = array[min];
+             array[min] = tmp;      
+        }
+    }
+    return array;}
