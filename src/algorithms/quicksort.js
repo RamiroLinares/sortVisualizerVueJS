604f185960f2396a8a5cd456def06e3b931c36cd
RamiroLinares <Ramiro.Linares@jala-foundation.org>
2021-11-24 16:32:16 -0300

    added vue bootstrap and quicksort

---
+++ src/algorithms/quicksort.js
@@ -0,0 +1,17 @@
+//recursive quicksort
+export default function quicksort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    var pivot = array[0];
+    
+    var left = []; 
+    var right = [];
+  
+    for (var i = 1; i < array.length; i++) {
+      array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+    }
+  
+    return quicksort(left).concat(pivot, quicksort(right));
+  };
---
+++ src/views/Quicksort.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="quicksort">
+    <h1>Quicksort</h1>
+    <b-button variant='primary' v-on:click="quicksort">Quicksort</b-button>
+  </div>
+</template>
+
+<script>
+import quicksort from '@/algorithms/quicksort.js'
+
+export default {
+  name: 'Quicksort',
+  methods: {
+    quicksort () {
+      const array = [4000, 10, 24, 1]
+      quicksort(array)
+    }
+  }
+}
+</script>
