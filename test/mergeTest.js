36e77410fb13ed4054a586d231da5ff599240ea7
Francisco Segura <fxsegura.dev@gmail.com>
2021-11-28 03:40:54 -0500

    Merge Sort Algorithm unit test

---
+++ test/mergeTest.js
@@ -0,0 +1,9 @@
+import {assert, expect} from 'chai';
+import mergesort from '../src/algorithms/mergesort.js';
+
+describe('Merge Sort',function(){
+    it('Algorithm should return array elements in order',function(){
+        let testArray=[10,6,7,5,3,4,8,9,2,1];
+        expect(mergesort(testArray)).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+    });
+});
