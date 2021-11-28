3f780b38b95320067cd4cf57d9ae18c1fdf959db
RamiroLinares <Ramiro.Linares@jala-foundation.org>
2021-11-28 20:26:23 -0300

    merge sort  added with visualizer

--- src/algorithms/mergesort.mjs
+++ src/algorithms/mergesort.mjs
@@ -1,28 +1,64 @@
-function merge(left, right) {
-    let arr = []
-    // Break out of loop if any one of the array gets empty
+/*function merge(left, right) {
+    let array = []
+    // Break out of loop if any one of the arrayay gets empty
     while (left.length && right.length) {
-        // Pick the smaller among the smallest element of left and right sub arrays 
+        // Pick the smaller among the smallest element of left and right sub arrayays 
         if (left[0] < right[0]) {
-            arr.push(left.shift())  
+            array.push(left.shift())  
         } else {
-            arr.push(right.shift()) 
+            array.push(right.shift()) 
         }
     }
     
     // Concatenating the leftover elements
-    // (in case we didn't go through the entire left or right array)
-    return [ ...arr, ...left, ...right ]
+    // (in case we didn't go through the entire left or right arrayay)
+    return [ ...array, ...left, ...right ]
 }
 
-export default function mergesort(array) {
-    const half = array.length / 2
+export default function mergesort(arrayay) {
+    const half = arrayay.length / 2
   
     // Base case or terminating case
-    if(array.length < 2){
-      return array 
+    if(arrayay.length < 2){
+      return arrayay 
     }
     
-    const left = array.splice(0, half)
-    return merge(mergesort(left),mergesort(array))
+    const left = arrayay.splice(0, half)
+    return merge(mergesort(left),mergesort(arrayay))
+  }*/
+
+export default function mergeSort(array) {
+    let list=[];
+    var sorted = array.slice(),
+        n = sorted.length,
+        buffer = new Array(n);
+  
+    for (var size = 1; size < n; size *= 2) {
+      for (var leftStart = 0; leftStart < n; leftStart += 2*size) {
+        var left = leftStart,
+            right = Math.min(left + size, n),
+            leftLimit = right,
+            rightLimit = Math.min(right + size, n),
+            i = left;
+        while (left < leftLimit && right < rightLimit) {
+          if (sorted[left] <= sorted[right]) {
+            buffer[i++] = sorted[left++];
+          } else {
+            buffer[i++] = sorted[right++];
+          }
+        }
+        while (left < leftLimit) {
+          buffer[i++] = sorted[left++];
+        }
+        while (right < rightLimit) {
+          buffer[i++] = sorted[right++];
+        }
+      }
+      var temp = sorted,
+          sorted = buffer,
+          buffer = temp;
+    
+       list.push(sorted); }
+  
+    return list;
   }
